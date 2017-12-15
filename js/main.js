3b0b196975b7d7cd4d752fc27243e06bb1620f62
Shanu S <shanusoman1987@gmail.com>
2017-12-15 18:00:57 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Main Menu
+$('.main-nav ul li:has(ul)').addClass('submenu');
+$('.main-nav ul li:has(ul)').append("<i></i>");
+$('.main-nav ul i').click(function() {
+    $(this).parent('li').toggleClass('open');
+    $(this).parent('li').children('ul').slideToggle();
+})
+
+//Mobile Menu
+$('.mob-btn').click(function() {
+    if (!$('html').hasClass('show-menu')) {
+        $('html').addClass('show-menu');
+    } else {
+        $('html').removeClass('show-menu');
+    }
+});
+
+$('.overlay').click(function() {
+    if ($('html').hasClass('show-menu')) {
+        $('html').removeClass('show-menu');
+    }
+});
+
+//Append and Prepend
+$('.first ul').clone().prependTo('.main-nav').addClass('desk-hide')
+$('.last ul').clone().appendTo('.main-nav').addClass('desk-hide')
